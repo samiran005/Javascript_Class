5f84cefee7c0cebd1f73df00482d02da40a45b00
Samiran Maity <maitysamiran442@gmail.com>
2023-04-15 00:49:40 +0530

    this is first

---
+++ Class 1/javascript.js
@@ -0,0 +1,13 @@
+/*
+"Javascipt" and "EcmaScript" is the same language.
+    Ecma is a company who made javaScript globaly excaptable to all browser by stadariging it.
+
+From ES5(EcmaScript version 5 / ES2016) it is called mordern Javascript.
+*/
+
+/*---------------------------------------------*/
+//1. how to connect your javascript file with html and what is the best way.
+//2. how to print on console.
+//3. comments lines
+//4. variable (let, var, const) and strict mode.
+//5. Data types and 'typeof' operator.
