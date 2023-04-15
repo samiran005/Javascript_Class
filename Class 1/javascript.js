24e1eef5023fb31feeb22a645d3458b15966cb01
Samiran Maity <maitysamiran442@gmail.com>
2023-04-15 16:55:31 +0530

    first class

--- Class 1/javascript.js
+++ Class 1/javascript.js
@@ -12,3 +12,35 @@ From ES5(EcmaScript version 5 / ES2016) it is called mordern Javascript.
 //4. variable (let, var, const) and strict mode.
 //5. Data types and 'typeof' operator.
 
+// console.log("hello world !!!")
+
+// const box = document.querySelector(".box1");
+
+// box.style.border = "5px solid black";
+
+// let x = 0;
+
+// window.addEventListener("keydown", (e)=>{
+//     console.log(e.key);
+
+//     if(e.key === 'w'){
+//         box.style.marginLeft = `${x ++}px`;
+//     }
+// })
+
+// console.dir(window)
+
+// var something = "nothing";
+
+// function func(){
+//     console.log("hi");
+// }
+
+// func();
+
+// // console.log(something);
+// console.log(func);
+
+
+console.log(document);
+
